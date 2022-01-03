f1f137fcc0abe98bff333e2426e721844bcdb522
robonxt-ai <56778225+robonxt-ai@users.noreply.github.com>
2022-01-03 02:05:19 -0800

    new year, new rewrite!
    rewriting code from scratch!

---
+++ BASE.001/BASE.001.ino
@@ -0,0 +1,117 @@
+/*
+    Main program for BASE.001
+    ------
+    A (start from small)™ program
+    ------
+    Designed for version: BASE.001.ino
+    Last modified: 2022.01.02
+    Programmer: robonxt
+    ------
+    2022.01.02: Reversioned to BASE.001. Due to changed servo positions, need to update servo control program
+    2022.01.03: Trying out SafeStringReader for reading serial data
+    ------
+    Goals:
+    code abides by C++ rules?
+*/
+
+#define VERSION "2022.01.03 2:00AM" // Increase this every time we upload?
+/*  ------------------------------------------------------------------------------------------------------  */
+// Default baudrates for each serial port (servo, serial, bluetooth) MUST BE INT
+#define SERVOC_BAUDRATE 115200      // SERVOC = Servo Controller
+#define SERIAL_BAUDRATE 115200      // SERIAL = Serial communication
+
+// Defines SoftwareSerial ports
+const byte PIN_SERVO_RX = D5;       // RX pin for the servo board
+const byte PIN_SERVO_TX = D6;       // TX pin for the servo board
+
+// For heartbeat
+const int LED_ON = LOW;             // Because LOW turns on the esp8266 built-in led
+const int LED_OFF = HIGH;           // Because HIGH turns off the esp8266 built-in led
+const int hbIntervalON = 70;        // How long to stay on?
+const int hbIntervalOFF = 1750;     // How long to stay off?
+bool hbSTATE = false;               // For heartbeat STATES
+
+// For startup settings
+bool isECHO = true;                 // Echo commands through serial?
+/*  ------------------------------------------------------------------------------------------------------  */
+// For AceRoutine (coroutines)
+#include <AceRoutine.h>
+using namespace ace_routine;
+
+// For Streaming (iostream)
+#include <Streaming.h>
+
+// For software serial ports
+#include <SoftwareSerial.h>
+SoftwareSerial  SerialServo(PIN_SERVO_RX, PIN_SERVO_TX);
+
+// For SafeString
+#include "SafeStringReader.h"
+createSafeStringReader(sfSerialReader, 50, " ,\r\n");
+createSafeStringReader(sfServoReader, 50, " ,\r\n");
+
+/*  ------------------------------------------------------------------------------------------------------  */
+// Include needed files (This should be just before the setup!)
+#include "xtra_classes.h"       // This includes the LineBuffer class
+#include "coroutines.h"         // This includes coroutines
+
+/*  ------------------------------------------------------------------------------------------------------  */
+// Objects
+//LineBuffer lineBuffer;  // Creates the linebuffer object for processing the serial data
+//serialReadConroutine serialRead;
+
+/*  ------------------------------------------------------------------------------------------------------  */
+
+/*  ------------------------------------------------------------------------------------------------------
+    [2022.01.02] setup > MUST HAVE
+    ------------------------------------------------------------------------------------------------------  */
+void setup() // Put your setup code here, to run once:
+{
+    // Begin Serial setup
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(D0, OUTPUT);
+
+    Serial.begin(SERIAL_BAUDRATE);
+    SerialServo.begin(SERVOC_BAUDRATE);
+    delay(200);
+    Serial << " " << endl << " " << endl;
+    SerialServo << " " << endl << " " << endl;
+    delay(10);
+    SafeString::setOutput(Serial);  // enable error messages and SafeString.debug() output to be sent to Serial
+    sfSerialReader.connect(Serial); // where SafeStringReader will read from
+    //sfServoReader.connect(Serial);  // where SafeStringReader will read from
+    if (isECHO)
+    {
+        sfSerialReader.echoOn();          // echo back all input, by default echo is off
+    }
+    Serial << "VERSION: " << VERSION << endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // End Serial setup
+    //
+    // Begin program setup
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    Serial << "---------- Setup START ----------" << endl;
+
+
+
+    Serial << "----------- Setup END -----------" << endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // End program setup
+    //
+    // Begin single run programs
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+/*  ------------------------------------------------------------------------------------------------------
+    [2021.06.16] loop > MUST HAVE
+    ------------------------------------------------------------------------------------------------------  */
+void loop()
+{
+    //
+    // Code for background services here
+    heartbeat.runCoroutine();       // Heartbeat
+    serialRead.runCoroutine();      // Read serial
+    //
+    // Code for normal program here
+}
---
+++ BASE.001/coroutines.h
@@ -0,0 +1,100 @@
+/*  ------------------------------------------------------------------------------------------------------
+    [2021.08.27] heartbeat > Heartbeat coroutine
+    ------------------------------------------------------------------------------------------------------  */
+COROUTINE(heartbeat)
+{
+    COROUTINE_LOOP()
+    {
+        digitalWrite(LED_BUILTIN, LED_ON);
+        digitalWrite(D0, LED_ON);
+        COROUTINE_DELAY(hbIntervalON);
+        digitalWrite(LED_BUILTIN, LED_OFF);
+        digitalWrite(D0, LED_OFF);
+        COROUTINE_DELAY(hbIntervalOFF);
+    }
+}
+
+/*  ------------------------------------------------------------------------------------------------------
+    [2022.01.03] serialRead > Serial coroutine
+    ------------------------------------------------------------------------------------------------------  */
+COROUTINE(serialRead)
+{
+    COROUTINE_LOOP()
+    {
+        if (sfSerialReader.read())
+        {
+            if (sfSerialReader == "start")
+            {
+                //handleStartCmd();
+            }
+            else if (sfSerialReader == "stop")
+            {
+                //handleStopCmd();
+            } // else ignore unrecognized command
+            else    // If none of the ifs above were true
+            {
+                Serial << "Not Recognized: " << sfSerialReader;
+            }
+            Serial << endl;
+            COROUTINE_DELAY(1);
+        } // else no delimited command yet
+        COROUTINE_DELAY(1);
+    }
+}
+
+/*  ------------------------------------------------------------------------------------------------------
+    [2022.01.02] serialReadConroutine > Coroutine for reading serial data
+    ------------------------------------------------------------------------------------------------------  */
+class serialReadConroutine: public Coroutine
+{
+    public:
+        int runCoroutine() override
+        {
+            COROUTINE_LOOP()
+            {
+                COROUTINE_AWAIT(Serial.available() > 0);
+                char c = Serial.read();
+                if (c == '\n' || c == '\r')
+                {
+                    const char* receivedChars = lineBuffer.getLine();
+                    Serial << "Received: " << receivedChars << endl;
+                    if (strcmp(receivedChars, "ok") == 0)           // Sets robot status to ok
+                    {
+                        Serial << "---------- ROBOT STATUS FORCED OK ----------" << endl;
+                    }
+                    //////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // Stuff here
+
+
+                    //////////////////////////////////////////////////////////////////////////////////////////////////////
+                    else if (strncmp(receivedChars, "send.", 5 ) == 0 )  // Direct control of servos (EXAMPLE: "send.#21P1500T200")
+                    {
+                        const char *sub_command = &receivedChars[5];
+                        SerialServo << sub_command << endl;
+                        Serial << SerialServo << endl;
+                    }
+                    //////////////////////////////////////////////////////////////////////////////////////////////////////
+                    ////////////////////// -------------------- STOP ALL SERVO -------------------- //////////////////////
+                    else if ((strcmp(receivedChars, "STOP") == 0) || (strcmp(receivedChars, "stop") == 0) || (strcmp(receivedChars, "KILL") == 0) || (strcmp(receivedChars, "kill") == 0))  // KILL SWITCH
+                    {
+                        Serial << "---------- KILL SWITCH ACTIVATED ----------" << endl;
+                        SerialServo << "#STOP" << endl;
+                    }
+                    ////////////////////// -------------------- STOP ALL SERVO -------------------- //////////////////////
+                    //////////////////////////////////////////////////////////////////////////////////////////////////////
+                    else    // If none of the ifs above were true
+                    {
+                        Serial << "Not Recognized" << endl;
+                    }
+                    Serial << "- done -" << endl;
+                    lineBuffer.clear();
+                }
+                else
+                {
+                    lineBuffer.append(c);
+                }
+            }
+        }
+    private:
+        LineBuffer lineBuffer;  // Creates the linebuffer object for processing the serial data
+};
